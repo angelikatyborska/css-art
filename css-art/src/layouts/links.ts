5ae01cb93226e4633fb81a7061bd82ea60e2c772
Angelika Tyborska <angelikatyborska@fastmail.com>
2024-07-10 19:35:23 +0200

    Bring back navigation

---
+++ css-art/src/layouts/links.ts
@@ -0,0 +1,35 @@
+// HEY!
+//
+// When adding new links, do not forget to take a screenshot of the canvas-wrapper node
+// and put it in public/covers!
+//
+
+export default {
+  grids: [
+    '/triangles',
+    '/triangles2',
+    '/lines',
+    '/lines2',
+    '/circles',
+    '/squares',
+    '/circles-squares',
+    '/hexes',
+    '/plaid',
+    '/pattern1',
+    '/pattern2',
+    '/pattern3',
+    '/pattern4_1',
+    '/pattern4_2',
+    '/beads',
+    '/eyes',
+    '/dot-queue',
+  ],
+  misc: [
+    '/flower',
+    '/hex',
+    '/sierpinski-triangle',
+    '/cassette',
+    '/100-gradients',
+    '/14-segment',
+  ],
+};
